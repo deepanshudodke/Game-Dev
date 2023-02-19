f192af6b12d24232148408c9b2850b3652703b05
not_deepanshu <deepanshudodke20012015@gmail.com>
2023-02-19 09:15:02 +0530

    completed

--- Playing Animation on Trigger/script.js
+++ Playing Animation on Trigger/script.js
@@ -8,25 +8,36 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y) { // Triggered or mouse coordinate
-        this.x = x;
-        this.y = y;
+
         this.spriteWidth = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth / 2;
-        this.height = this.spriteHeight / 2;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+        this.x = x - this.width / 2;
+        this.y = y - this.height / 2;
         this.image = new Image();
         this.image.src = 'boom.png';
         this.frame = 0;
+        this.timer = 0;
+        this.sound = new Audio();
+        this.sound.src = 'boom.wav';
 
     }
 
     update() {
-        this.frame++;
+        if (this.frame == 0) {
+            this.sound.play();
+        }
+        this.timer++;
+        if (this.timer % 10 == 0) {
+            this.frame++;
+        }
+
 
     }
 
     draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
         ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
@@ -38,9 +49,14 @@ window.addEventListener('click', function (e) {
 })
 
 function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     for (let i = 0; i < explosions.length; i++) {
         explosions[i].update();
         explosions[i].draw();
+        if (explosions[i].frame > 5) {
+            explosions.splice(i, 1);
+            i--;
+        }
     }
     requestAnimationFrame(animate);
 }
